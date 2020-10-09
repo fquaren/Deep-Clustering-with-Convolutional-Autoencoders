981148d62083dc26b968dbdc526bd6fb2b550204
Filippo Quarenghi <filippo.quarenghi@gmail.com>
2020-10-09 19:23:49 +0200

    pulizie di inizio inverno

--- src/metrics.py
+++ src/metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.metrics import normalized_mutual_info_score, adjusted_rand_score
+from scipy.optimize import linear_sum_assignment as linear_assignment
 
 nmi = normalized_mutual_info_score
 ari = adjusted_rand_score
@@ -20,6 +21,5 @@ def acc(y_true, y_pred):
     w = np.zeros((D, D), dtype=np.int64)
     for i in range(y_pred.size):
         w[y_pred[i], y_true[i]] += 1
-    from sklearn.utils.linear_assignment_ import linear_assignment
-    ind = linear_assignment(w.max() - w)
-    return sum([w[i, j] for i, j in ind]) * 1.0 / y_pred.size
+    row_ind, col_ind = linear_assignment(w.max() - w)
+    return sum([w[i, j] for (i, j) in zip(row_ind, col_ind)]) * 1.0 / y_pred.size
