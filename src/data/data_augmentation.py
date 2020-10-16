91ae93af90274dd8d19ea1fd5a87537779b5b975
fquaren <filippo.quarenghi@gmail.com>
2020-10-16 21:42:01 +0200

    import data and data augmentation

---
+++ src/data/data_augmentation.py
@@ -0,0 +1,95 @@
+from tqdm import tqdm
+import os
+import random
+from os import listdir
+from os.path import isfile, join
+import imageio
+import skimage
+import skimage.io
+import skimage.transform
+from scipy import ndarray
+
+
+def random_rotation(image_array: ndarray):
+    '''
+    Pick a random degree of rotation between 25% on the left and 25% on the 
+    right.
+    '''
+    random_degree = random.uniform(-25, 25)
+    return skimage.transform.rotate(image_array, random_degree)
+
+
+def horizontal_flip(image_array: ndarray):
+    '''
+    Horizontal flipping of the image array.
+    '''
+    return image_array[:, ::-1]
+
+
+def vertical_flip(image_array: ndarray):
+    '''
+    Vertical flipping of the image array.
+    '''
+    return image_array[::-1, :]
+
+
+def data_aumgentation(j, processed_data, processed_dirs, train_file_names):
+    '''
+    To data aumgentation on train_file_names (list of image names) saving them
+    in processed_data+processed_dirs[0] ('data/processed/train').
+    '''
+    # our folder path containing some images
+    folder_path = os.path.join(processed_data, processed_dirs[0])
+    # the number of file to generate
+    num_files_desired = (len(train_file_names))*j
+    # loop on all files of the folder and build a list of files paths
+    images = [os.path.join(folder_path, f) for f in os.listdir(
+        folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    available_transformations = {
+        'rotate': random_rotation,
+        'horizontal_flip': horizontal_flip,
+        'vertical_flip': vertical_flip
+    }
+    # TODO fix
+    # https://stackoverflow.com/questions/58893860/lossy-conversion-from-float64-to-uint8
+    for i in tqdm(range(num_files_desired)):
+        # random image from the folder
+        image_path = random.choice(images)
+        # read image as an two dimensional array of pixels
+        image_to_transform = skimage.io.imread(image_path)
+
+        # choose a random transformation to apply for a single image
+        key = random.choice(list(available_transformations))
+        transformed_image = available_transformations[key](image_to_transform)
+
+        # define a name for our new file
+        image_path = image_path.split('/')[-1]
+        new_file_path = '{}_augmented_image_{}.png'.format(
+            image_path.split('.')[0], i)
+        # TODO fix here probably
+        # img_uint8 = transformed_image.astype(np.uint8)
+        imageio.imwrite(
+            os.path.join(folder_path, new_file_path), transformed_image)
+        # write image to the disk
+        # sk.io.imsave(new_file_path, transformed_image)
+
+
+def check_data(
+        processed_data, processed_dirs, val_file_names, test_file_names):
+    '''
+    Print percentage images in train, val and test.
+    '''
+    train_file_names = [
+        f for f in listdir(os.path.join(processed_data, processed_dirs[0])) 
+        if isfile(join(os.path.join(processed_data, processed_dirs[0]), f))
+    ]
+    # print dimensions datasets
+    lenTot = len(train_file_names)+len(val_file_names)+len(test_file_names)
+    percTrain = (len(train_file_names)/lenTot)*100
+    percVal = (len(val_file_names)/lenTot)*100
+    percTest = (len(test_file_names)/lenTot)*100
+    print('Number of train images:', len(
+        train_file_names), '= %.0f' % percTrain, '%')
+    print('Number of validation images:', len(
+        val_file_names), '= %.0f' % percVal, '%')
+    print('Number of test images:', len(test_file_names), '= %.0f' % percTest, '%')
---
+++ src/data/import_images.py
@@ -0,0 +1,119 @@
+import os
+import shutil
+from os import listdir
+from os.path import isfile, join
+from sklearn.model_selection import train_test_split
+from data_augmentation import data_aumgentation, check_data
+
+
+def copyImages(sources, _file_names, directory):
+    for source in sources:
+        for f in _file_names:
+            try:
+                shutil.copy(os.path.join(source, f), directory)
+            except OSError:
+                pass
+    print('Copied.')
+
+
+# SETTINGS
+
+# Images
+
+# raw data paths
+CT = '../../data/raw/CT/'
+MRI = '../../data/raw/MRI/'
+PET = '../../data/raw/PET/'
+train_size = 0.5
+val_size = 0.7
+
+# Data augmentation
+
+# 0.7, 0.2 senza augmentation
+# 0.4, 0.4 con augmentation (j=2)
+# 0.3, 0.4 con j=3
+j = 2  # scale of augmentation
+
+# ------------------------------------------------------------------------------
+
+# IMPORT IMAGES
+
+myPaths = [CT, MRI, PET]
+myDict = {CT: [], MRI: [], PET: []}
+for path in myPaths:
+    myDict[path] = [f for f in listdir(path) if isfile(join(path, f))]
+
+
+# Split data in train, validation and test datasets
+train_size = 0.5
+val_size = 0.7
+# split train test
+CT_train, CT_test = train_test_split(
+    myDict[myPaths[0]], train_size=train_size)
+MRI_train, MRI_test = train_test_split(
+    myDict[myPaths[1]], train_size=train_size)
+PET_train, PET_test = train_test_split(
+    myDict[myPaths[2]], train_size=train_size)
+# split train validation
+CT_val, CT_test = train_test_split(CT_test, train_size=val_size)
+MRI_val, MRI_test = train_test_split(MRI_test, train_size=val_size)
+PET_val, PET_test = train_test_split(PET_test, train_size=val_size)
+# create list of file names
+train_file_names = CT_train + MRI_train + PET_train
+val_file_names = CT_val + MRI_val + PET_val
+test_file_names = CT_test + MRI_test + PET_test
+# print dimensions datasets
+lenTot = len(train_file_names)+len(val_file_names)+len(test_file_names)
+percTrain = (len(train_file_names)/lenTot)*100
+percVal = (len(val_file_names)/lenTot)*100
+percTest = (len(test_file_names)/lenTot)*100
+print('Number of train images:', len(
+    train_file_names), '= %.0f' % percTrain, '%')
+print('Number of validation images:', len(
+    val_file_names), '= %.0f' % percVal, '%')
+print('Number of test images:', len(test_file_names), '= %.0f' % percTest, '%')
+
+
+# Make processed and copy images in train, val and test
+# delete existing processed data
+processed_data = '../../data/processed'
+if os.path.exists(processed_data):
+    print('Deleting previous data in {}'.format(processed_data))
+    try:
+        shutil.rmtree(processed_data)
+    except:
+        raise
+print('Delete operation completed successfully.')
+# make preprocessed/
+print('Creating processed data directories...')
+processed_dirs = [
+    'train',
+    'val',
+    'test'
+]
+for d in processed_dirs:
+    os.makedirs(os.path.join(processed_data, d))
+    print('created ', os.path.join(processed_data, d))
+
+# Copy images in relative dir
+_file_names = [train_file_names, val_file_names, test_file_names]
+for name, directory in zip(_file_names, processed_dirs):
+    copyImages(myPaths, name, os.path.join(processed_data, directory))
+# check
+struct = ['training dataset', 'validation dataset', 'test dataset']
+for d, name, s in zip(processed_dirs, _file_names, struct):
+    a = len(
+        [name for name in os.listdir(os.path.join(processed_data, d))
+            if os.path.isfile(os.path.join(os.path.join(processed_data, d), name))])
+    if a != len(name):
+        print('ERROR, i numberi non combaciano in ', s)
+    else:
+        print(s, 'OK.')
+
+# ------------------------------------------------------------------------------
+
+# DATA AUGMENTATION
+
+data_aumgentation(j, processed_data, processed_dirs, train_file_names)
+check_data(
+    processed_data, processed_dirs, val_file_names, test_file_names)
